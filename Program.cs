872e01dde5f30efa74b609bec25a27583c5d3d13
OLIPrtn <114662491+OLIPrtn@users.noreply.github.com>
2022-09-30 14:46:38 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace MyApp
         static void Main(string[] args)
         {
             // string for current day that will take the currentday from the system and parsing it to a string. 
-            // Tostring was the only thing that kept me from completing this faster until I saw it on msft docs link
             string currentDay = DateTime.Today.DayOfWeek.ToString();
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Please enter day of week: ");
@@ -15,6 +14,7 @@ namespace MyApp
             string userInput = Console.ReadLine();
 
             // if statement that will see if the UserInput variable data matches the currentDay string data
+            // There is a much better method, perhaps using a boolean to see if it matches currentDay
             if (userInput == currentDay)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
